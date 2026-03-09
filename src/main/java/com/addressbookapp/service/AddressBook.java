26ff422fb3ca1633006a1697095cd55e020ecd79
Khushi Agrawal <agrawalkhushi267@gmail.com>
2026-03-09 14:22:20 +0530

    [KhushiAgrawal] Added : UC4

--- src/main/java/com/addressbookapp/service/AddressBook.java
+++ src/main/java/com/addressbookapp/service/AddressBook.java
@@ -1,14 +1,75 @@
 package com.addressbookapp.service;
 
-
 import com.addressbookapp.model.Person;
+import java.util.ArrayList;
+import java.util.Optional;
+import java.util.Scanner;
 
 public class AddressBook {
 
-    public void addContact(Person person) {
+	// List to store contacts
+	ArrayList<Person> contactList = new ArrayList<>();
 
-        System.out.println("Contact Added Successfully");
-        System.out.println(person);
+	// Method to add contact
+	public void addContact(Person person) {
+		contactList.add(person);
+		System.out.println("Contact Added Successfully");
+	}
 
-    }
+	// Method to edit contact using first name
+	public void editContact(String name) {
+
+		Scanner scanner = new Scanner(System.in);
+
+		// Find person using Stream API
+		Optional<Person> personOptional = contactList.stream()
+				.filter(person -> person.getFirstName().equalsIgnoreCase(name)).findFirst();
+
+		if (personOptional.isPresent()) {
+
+			Person person = personOptional.get();
+
+			System.out.print("Enter new Address: ");
+			person.setAddress(scanner.nextLine());
+
+			System.out.print("Enter new City: ");
+			person.setCity(scanner.nextLine());
+
+			System.out.print("Enter new State: ");
+			person.setState(scanner.nextLine());
+
+			System.out.print("Enter new Zip: ");
+			person.setZip(scanner.nextLine());
+
+			System.out.print("Enter new Phone Number: ");
+			person.setPhoneNumber(scanner.nextLine());
+
+			System.out.print("Enter new Email: ");
+			person.setEmail(scanner.nextLine());
+
+			System.out.println("Contact Updated Successfully");
+
+		} else {
+			System.out.println("Contact not found");
+		}
+	}
+	
+	//UC:4- Method to delete contact using name
+	public void deleteContact(String name) {
+
+	    // Find person using Stream API
+	    Optional<Person> personOptional = contactList.stream()
+	            .filter(person -> person.getFirstName().equalsIgnoreCase(name))
+	            .findFirst();
+
+	    if (personOptional.isPresent()) {
+
+	        contactList.remove(personOptional.get());
+	        System.out.println("Contact Deleted Successfully");
+
+	    } else {
+
+	        System.out.println("Contact not found");
+	    }
+	}
 }
