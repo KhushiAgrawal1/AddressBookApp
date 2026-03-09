051d695fb75aff3417b3744e064e6882d5192e1c
Khushi Agrawal <agrawalkhushi267@gmail.com>
2026-03-09 11:16:10 +0530

    [KhushiAgrawal] Added : UC1

---
+++ src/main/java/com/addressbookapp/AddressBookMainApplication.java
@@ -0,0 +1,14 @@
+package com.addressbookapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AddressBookMainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AddressBookMainApplication.class, args);
+		System.out.println("Welcome to Address Book Program");
+	}
+
+}
