d6c7dabacff57d7359517b42498fb04fbddc7e41
Khushi Agrawal <agrawalkhushi267@gmail.com>
2026-03-09 11:24:04 +0530

    [KhushiAgrawal] Added : UC2

--- src/main/java/com/addressbookapp/AddressBookMainApplication.java
+++ src/main/java/com/addressbookapp/AddressBookMainApplication.java
@@ -1,14 +1,54 @@
 package com.addressbookapp;
 
+import java.util.Scanner;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.addressbookapp.model.Person;
+import com.addressbookapp.service.AddressBook;
 
 @SpringBootApplication
 public class AddressBookMainApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AddressBookMainApplication.class, args);
+
+		Scanner scanner = new Scanner(System.in);
+
 		System.out.println("Welcome to Address Book Program");
+
+		System.out.print("Enter First Name: ");
+		String firstName = scanner.nextLine();
+
+		System.out.print("Enter Last Name: ");
+		String lastName = scanner.nextLine();
+
+		System.out.print("Enter Address: ");
+		String address = scanner.nextLine();
+
+		System.out.print("Enter City: ");
+		String city = scanner.nextLine();
+
+		System.out.print("Enter State: ");
+		String state = scanner.nextLine();
+
+		System.out.print("Enter Zip: ");
+		String zip = scanner.nextLine();
+
+		System.out.print("Enter Phone Number: ");
+		String phone = scanner.nextLine();
+
+		System.out.print("Enter Email: ");
+		String email = scanner.nextLine();
+
+		// UC:2
+		Person person = new Person(firstName, lastName, address, city, state, zip, phone, email);
+
+		AddressBook addressBook = new AddressBook();
+
+		addressBook.addContact(person);
+
 	}
 
 }
---
+++ src/main/java/com/addressbookapp/service/AddressBook.java
@@ -0,0 +1,14 @@
+package com.addressbookapp.service;
+
+
+import com.addressbookapp.model.Person;
+
+public class AddressBook {
+
+    public void addContact(Person person) {
+
+        System.out.println("Contact Added Successfully");
+        System.out.println(person);
+
+    }
+}
